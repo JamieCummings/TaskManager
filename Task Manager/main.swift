961c7cbd0e5480f764f5082250d62f03093b5748
Jamie Cummings <jamiecummings@macbook-pro-2.local>
2018-09-12 10:07:47 -0500

    Initial Commit

---
+++ Task Manager/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Task Manager
+//
+//  Created by Jamie Cummings on 9/12/18.
+//  Copyright © 2018 Jamie Cummings. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
